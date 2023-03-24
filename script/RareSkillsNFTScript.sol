2390b8133c6d6337728fca4205b6bb827bebf9ee
f-hannoun <fhannoun@proton.me>
2023-03-24 12:09:40 +0100

    changing deployment script name

--- script/RareSkillsNFTScript.sol
+++ script/RareSkillsNFTScript.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "../src/RareSkillsNFT.sol";
 
-contract SpacebearScript is Script {
+contract RareSkillsNFTDeploy is Script {
     function setUp() public {}
 
     function run() public {
